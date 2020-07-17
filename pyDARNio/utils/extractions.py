e0417e9dc8b58d4cd7d32b00bffc31a5da8b1c23
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-07-17 11:06:01 -0400

    ENH: time series extraction routines
    Routines to:
    - Build datetime objects from dmap and other records
    - Extract time series data from records

---
+++ pyDARNio/utils/extractions.py
@@ -0,0 +1,170 @@
+# Copyright 2020 
+# Author: Angeline G. Burrell, U.S. Naval Research Laboratory (NRL)
+
+"""
+This module contains methods to extract time series and scans from data dicts
+"""
+
+import datetime as dt
+import logging
+import numpy as np
+from typing import Dict, List
+
+pyDARNio_log = logging.getLogger('pyDARNio')
+
+def datetime_from_records(records: List[dict], yr_key='time.yr',
+                          mo_key='time.mo', dy_key='time.dy', hr_key='time.hr',
+                          mt_key='time.mt', sc_key='time.sc',
+                          us_key='time.us'):
+    """ Create a datetime object from dictionary values
+
+    Parameters
+    ----------
+    records : List[dict]
+        list of records contained in dictionaries
+    yr_key : str
+        Year key (default='time.yr')
+    mo_key : str
+        Month key (default='time.mo')
+    dy_key : str
+        Day of month key (default='time.dy')
+    hr_key : str or NoneType
+        Hour of day key or None if not available (default='time.hr')
+    mt_key : str or NoneType
+        Minutes of hour or None if not available (default='time.mt')
+    sc_key : str or NoneType
+        Seconds of minute or None if not available (default='time.sc')
+    us_key : str or NoneType
+        Microseconds of seconds or None if not available (default='time.us')
+
+    Returns
+    -------
+    dtimes : List[dt.datetime]
+        a list of datetime objects, containing the datetimes for each
+        record in the input list
+
+    Notes
+    -----
+    If None is used for any time key, the smaller increment keys are ignored.
+    Default key values are the appropriate time values for dmap records.
+
+    Raises
+    ------
+    ValueError
+        When year, month, or day keys are specified
+    KeyError
+        When a time key is missing from the data record
+
+    """
+
+    # Get the time keys that will be used
+    time_keys = list()
+    time_fmts = ["%Y", "%m", "%d", "%H", "%M", "%S", "%f"]
+    for tkey in [yr_key, mo_key, dy_key, hr_key, mt_key, sc_key, us_key]:
+        if tkey is None:
+            break
+        time_keys.append(tkey)
+
+    # Test the input
+    if len(time_keys) < 3:
+        raise ValueError('need a minimum of year, month, and day as input')
+
+    # Build the datetime format string
+    dtime_fmt = " ".join(time_fmts[:len(time_keys)])
+
+    # Build the time string and datetime objects for each dict
+    dtimes = list()
+    for dmap_dict in dmap_records:
+        # Get the datetime after building the time string
+        dtime_str = " ".join([str(dmap_dict[tkey]) for tkey in time_keys])
+        dtimes.append(dt.datetime.strptime(dtime_str, dtime_fmt))
+
+    return dtimes
+
+def time_series_from_records(records: List[dict], target_keys: List[str],
+                             time_kwargs={}, time_key='rec_time'):
+    """ Extract lists of data values as time series
+
+    Parameters
+    ----------
+    records : List[dict]
+        list of records contained in dictionaries
+    target_keys : List[str]
+        List of keys to extract as a time series from the records
+    time_kwargs : Dict
+        Dictionary of time keyword arguements used as optional inputs for
+        datetime_from_records (default={})
+    time_key : str
+        Output dictionary key name for the datetime values (default='rec_time')
+
+    Returns
+    -------
+    time_series : Dict[np.array]
+        Dict of lists that contain the time-series data from the target keys
+        plus time, which has the key specified in the input parameter `time_key`
+
+    Raises
+    ------
+    ValueError
+        If target data values have sizes that cannot be reconciled into a
+        time series (e.g., two multi-dimensional data sets that have different
+        lenths)
+
+    Notes
+    -----
+    Will skip records that do not contain all target keys
+
+    """
+
+    # Get the datetime of each record
+    rec_time = datetime_from_records(records, **time_kwargs)
+
+    if len(target_keys) == 0:
+        return {time_key: np.asarray(rec_time)}
+
+    # Initialize the output
+    time_series = {tkey: list() for tkey in target_keys}
+    time_series[time_key] = list()
+
+    # For each record, get the desire data values
+    for irec, dmap_dict in enumerate(records):
+        # Get the shape of the records at this time, also test that all
+        # data keys are present.  If not, do not save data from this record
+        try:
+            rec_lens = np.unique([1 if np.asarray(dmap_dict[tkey]).shape == ()
+                                  else np.asarray(dmap_dict[tkey]).shape[0]
+                                  for tkey in target_keys])
+        except KeyError as kerr:
+            pyDARNio_log.info("missing key(s) at record {:d}: {:}".format(
+                irec, kerr))
+            continue
+
+        if len(rec_lens) > 2:
+            raise ValueError('target data types have different sizes')
+        max_rec = rec_lens.max()
+
+        # Save the output data in lists the length of the maximum record length
+        for tkey in target_keys:
+            if max_rec == 1:
+                time_series[tkey].append(dmap_dict[tkey])
+            else:
+                if rec_data[tkey].shape == ():
+                    time_series[tkey].extend([dmap_dict[tkey]
+                                              for i in range(max_rec)])
+                else:
+                    time_series[tkey].extend(list(dmap_dict[tkey]))
+
+        # Save the time data for this record
+        if max_rec == 1:
+            time_series[time_key].append(rec_time[irec])
+        else:
+            time_series[time_key].extend([rec_time[irec]
+                                          for i in range(max_rec)])
+
+    # Recast the time series lists as numpy arrays
+    for tkey in time_series.keys():
+        time_series[tkey] = np.asarray(time_series[tkey])
+
+    return time_series
+            
+            
