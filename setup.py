74b639c77dcd36789da192132b2333644bd19eb5
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-07-17 08:47:47 -0400

    BUG: long descritption
    Added a missing definition of `long_description`, allowing setup.py to run.

--- setup.py
+++ setup.py
@@ -17,6 +17,10 @@ from subprocess import check_call
 from setuptools.command.install import install, orig
 
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 # Setup information
 setup(
     name="pyDARNio",
