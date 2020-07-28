e0bc57beb0f16d4f5808186c5ba2f3f71e8eb2ea
mts299 <marina.t.schmidt@gmail.com>
2020-07-28 16:16:41 -0600

    converting pyDARNio to pydarnio in importting

--- pydarnio/__init__.py
+++ pydarnio/__init__.py
@@ -17,7 +17,6 @@ import os
 from .exceptions import dmap_exceptions
 from .exceptions import superdarn_exceptions
 from .exceptions import borealis_exceptions
-from .exceptions import radar_exceptions
 from .exceptions.warning_formatting import standard_warning_format
 from .exceptions.warning_formatting import only_message_warning_format
 
--- pydarnio/borealis/base_format.py
+++ pydarnio/borealis/base_format.py
@@ -41,7 +41,7 @@ import numpy as np
 
 from collections import OrderedDict
 
-from pyDARNio import borealis_exceptions
+from pydarnio import borealis_exceptions
 
 
 class BaseFormat():
