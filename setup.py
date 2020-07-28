cc83899620e45917dd1be92667e7b53f60037bf3
mts299 <marina.t.schmidt@gmail.com>
2020-07-28 16:17:53 -0600

    added in long discription code

--- setup.py
+++ setup.py
@@ -16,6 +16,10 @@ import sys
 from subprocess import check_call
 from setuptools.command.install import install, orig
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 # Setup information
 setup(
